49d241f0f8936054dc97740ce694b59b242e5aa9
Jessica Vazquez <contact@jmvazquez.com>
2016-11-07 21:55:29 -0400

    Added an order by rule to scores query. Rows will be returned from highest to lowest score value.

--- api/index.php
+++ api/index.php
@@ -58,10 +58,13 @@ Flight::route('POST /scores', function() use($db) {
 Flight::route('GET /scores/@game_id:[0-9]+(/@limit:[0-9]+)', function($game_id, $limit) use($db) {
 	
 	// Establish statement
-	$query = 'SELECT * FROM scores WHERE game_id='. $game_id;
+	$query = 'SELECT * FROM scores WHERE game_id='. $game_id .' ';
+	$query .= 'ORDER BY value DESC ';
 	if ($limit > 0) {
 		$query .= 'LIMIT '. $limit;
 	}
+
+	echo $query;
 
 	$statement = $db->prepare($query);
 
